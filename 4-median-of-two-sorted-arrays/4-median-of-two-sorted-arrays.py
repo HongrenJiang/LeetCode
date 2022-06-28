09ae80c625d20b7d884782fc08f83a14324f77e8
HongrenJiang <90483616+HongrenJiang@users.noreply.github.com>
2022-06-28 10:40:36 -0400

    Time: 229 ms (5.44%), Space: 14.3 MB (24.88%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -3,16 +3,17 @@ class Solution:
         A, B = nums1, nums2
         if(len(A) > len(B)):
             A, B = B, A
-        total = len(nums1) + len(nums2)
+        total = len(A) + len(B)
         half = total // 2 # half of the total (even) or (half -1) of the total (odd)
         l, r = 0, len(A) -1 # binary search index
         
         while True:
             i = (l + r) // 2 # median index of binary search (or the left one)
-            j = half - (i + 1) - 1
+            j = half - i - 2
             # (i + 1): how many numbers in the left partition of A
             # half - (i + 1) how many numbers in the left partition of B
             # half - (i + 1) -1 transfer it into index
+            # j = half - i - 2
             
             leftA = A[i] if i >= 0 else float("-infinity")
             rightA = A[i + 1] if (i + 1) < len(A) else float("infinity")
