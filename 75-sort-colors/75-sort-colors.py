71f37312a049c00a0be32d72494bbce2b50b2966
HongrenJiang <90483616+HongrenJiang@users.noreply.github.com>
2022-07-18 15:35:12 -0400

    Time: 60 ms (30.60%), Space: 13.5 MB (99.96%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,4 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        return nums.sort()
+        ## return nums.sort()
+        ## Bucket Sort Solution
+        bucket_dict = {0: 0, 1: 0, 2: 0}
+        for num in nums:
+            bucket_dict[num] += 1
+        
+        count_zero = bucket_dict[0]
+        count_one = bucket_dict[1]
+        count_two = bucket_dict[2]
+        
+        nums[0:count_zero] = [0] * count_zero
+        nums[count_zero: count_zero + count_one] = [1] * count_one
+        nums[count_zero + count_one:] = [2] * count_two
+        
+        return nums
