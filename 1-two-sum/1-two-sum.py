fab028df218200ae386c41cf47dc6c1c78ed92a4
HongrenJiang <90483616+HongrenJiang@users.noreply.github.com>
2022-07-24 21:40:19 -0400

    Time: 62 ms (95.43%), Space: 15.1 MB (50.46%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,7 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Method 1: Brute Force
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # Method 2: One-Pass Hash Table (Find the diff from the past)
+        # Time Complexity: O(n)
+        # Space Complexity: O(n)
+        hashMap = { } # val -> index
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in hashMap:
+                return [i, hashMap[diff]]
+            hashMap[num] = i
+        
+        # # Method 1: Brute Force
+        # # Time Complexity: O(C(n,2)) = O(n(n-1)/2) = O(n^2)
+        # # Space Complexity: O(1)
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
